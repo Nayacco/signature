62e30dfdbe303f61251e87a27e5eadb6a610f563
suikelei <sui.kelei@htnova.com>
2020-01-08 15:08:39 +0800

    Initial commit

---
+++ src/main/java/com/htnova/signature/SignatureApplication.java
@@ -0,0 +1,13 @@
+package com.htnova.signature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignatureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/htnova/signature/SignatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htnova.signature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
